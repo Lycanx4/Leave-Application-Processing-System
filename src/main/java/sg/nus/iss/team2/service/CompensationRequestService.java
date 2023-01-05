712856571085c4d4b81aff13a536ba3cda678db7
Lycanx4 <51407306+Lycanx4@users.noreply.github.com>
2023-01-05 20:20:10 +0800

    Change Repo

---
+++ src/main/java/sg/nus/iss/team2/service/CompensationRequestService.java
@@ -0,0 +1,26 @@
+package sg.nus.iss.team2.service;
+
+import java.util.List;
+
+import sg.nus.iss.team2.model.CompensationRequest;
+import sg.nus.iss.team2.model.Employee;
+
+public interface CompensationRequestService {
+
+    List<CompensationRequest> findAllCompensationRequest();
+
+    List<CompensationRequest> findEmployeeCompensationRequest(Employee employee);
+
+    CompensationRequest findCompensationRequest(Long compensationRequestId);
+
+    CompensationRequest createCompensationRequest(CompensationRequest compensationRequest);
+
+    CompensationRequest updCompensationRequest(CompensationRequest compensationRequest);
+
+    void removeCompensationRequest(CompensationRequest compensationRequest);
+
+    List<CompensationRequest> findRequestByTeam(List<Employee> team);
+
+    void updateCompReqAndLeaveBalance(CompensationRequest compensationRequest, String decision, String comment);
+
+}
